2e58c643791be0648739ba83fd0db9da8ce0bc11
franco@mumuki.org <franco@mumuki.org>
2023-01-14 15:31:29 +0000

    Mumuki Export on 2023-01-14 15:31:29 +0000

--- 00003_Tudo se transforma/extra.py
+++ 00003_Tudo se transforma/extra.py
@@ -1,7 +1,7 @@
 livros = ["Ensaio sobre a Cegueira", "Socorro", "Meu Pé de Laranja Lima"]
 
 def pronto():
-  if "Fundação" in libros:
+  if "Fundação" in livros:
     return "Muito bem!"
   else:
     raise RuntimeError("Não se esqueça de adicionar 'Fundamentos' à lista de livros!")
